742ad6ccca20f61383c5978a57340ded7b47b750
Gronyon <gronyon@gmail.com>
2020-04-21 20:31:10 +0200

    Attempt to use classes in javascript.

--- ShowToChat.js
+++ ShowToChat.js
@@ -16,15 +16,15 @@ var ShowToChatScript = ShowToChatScript || (function() {
   			_handleShowMe
   		);
 
-      // apicmd.on(
-  		// 	'show-selected',
-  		// 	'Show a selection to all players.',
-  		// 	'--xx xxx',
-  		// 	[
-  		// 		['-x', '--xxx TEXT', 'Some option.']
-  		// 	],
-  		// 	_handleShowSelected
-  		// );
+      apicmd.on(
+  			'show-pic',
+  			'Show a token or character picture to given players.',
+  			'[--players TEXT]',
+  			[
+  				['-p', '--players TEXT', '"all" to show to all players, or comma separated player names or ids.']
+  			],
+  			_handleShowPicture
+  		);
     }
 
     function showToPlayers(sendFrom, name, avatar, notes)
@@ -35,9 +35,9 @@ var ShowToChatScript = ShowToChatScript || (function() {
       sendChat(fromName, `${fullHtml}`);
     }
 
-    function whisperToPlayer(sendFrom, sendTo, name, avatar, notes)
+    function whisperToPlayer(sendFrom, sendTo, data)
     {
-      var fullHtml = _createHtmlMessage(name, avatar, notes);
+      var fullHtml = data.getHtml();
       var fromName = _getPlayerName(sendFrom);
       var toName = _getPlayerName(sendTo);
 
@@ -47,6 +47,47 @@ var ShowToChatScript = ShowToChatScript || (function() {
     // ---------------------------------------------
     // PRIVATE
     // ---------------------------------------------
+    function _handleShowPicture(argv, msg)
+    {
+      var obj = new ObjectData("name", "avatar", "text");
+      var html = obj.getHtml();
+      log (html);
+
+
+      if (!argv.opts.players) {
+        // players not given
+        // Use the selected tokens to pic a player list
+        // And display a "select token" button to the chat.
+
+      } else {
+        var players = [];
+        if ("all" == argv.opts.players) {
+          // show selected tokens to all players
+          _.chain(msg.selected)
+            .map(function(o) {
+              return getObj('graphic',o._id);
+            })
+            .compact()
+            .each(function(token) {
+              var character = null;
+              var represents = token.get('represents');
+              if (represents) {
+                character = getObj('character', represents);
+              }
+              if (character) {
+                _showCharacterToPlayer(msg.playerid, null, character);
+              } else {
+                _showTokenToPlayer(msg.playerid, null, token);
+              }
+            }
+          );
+
+        } else {
+          // show to specific list of players
+        }
+      }
+    }
+
     function _handleShowMe(argv, msg)
   	{
       if (argv.opts.handout) {
@@ -57,8 +98,9 @@ var ShowToChatScript = ShowToChatScript || (function() {
           sendChat("api", `/w gm Handout ${argv.opts.handout} not found.`);
           return;
         }
-
-        _showHandoutToPlayer(msg.playerid, msg.playerid, handout);
+        _collectHandout(handout, function(data) {
+          whisperToPlayer(msg.playerid, msg.playerid, data);
+        });
       } else {
 
         // Whisper selected tokens (or characters represented by those tokens).
@@ -74,14 +116,61 @@ var ShowToChatScript = ShowToChatScript || (function() {
               character = getObj('character', represents);
             }
             if (character) {
-              _showCharacterToPlayer(msg.playerid, msg.playerid, character);
+              _collectCharacter(character, function(data) {
+                whisperToPlayer(msg.playerid, msg.playerid, data);
+              });
             } else {
-              _showTokenToPlayer(msg.playerid, msg.playerid, token);
+              _collectToken(token, function(data) {
+                whisperToPlayer(msg.playerid, msg.playerid, data);
+              });
             }
-          }
-        );
-
+          });
       }
+    }
+
+    function _collectHandout(handout, callback)
+    {
+      var name = handout.get('name');
+      var avatar = handout.get('avatar');
+      var notes = null;
+      var gmnotes = null;
+      handout.get('notes', function(returnedNotes) {
+        notes = returnedNotes;
+        handout.get('gmnotes', function(returnedGmnotes) {
+          gmnotes = returnedGmnotes;
+          let handoutData = new ObjectData(name, avatar, notes, gmnotes);
+          callback(handoutData);
+        });
+      });
+    }
+
+    function _collectToken(token, callback)
+    {
+      var name = token.get('name');
+      var avatar = token.get('avatar');
+      var notes = null; // tokens have no public "notes" of their own.
+      // for some reason, on tokens, gmnotes are not asynchronous.
+      var gmnotes = token.get('gmnotes');
+      // but they are url encoded!
+      gmnotes = unescape(gmnotes);
+      let tokenData = new ObjectData(name, avatar, notes, gmnotes);
+      callback(tokenData);
+    }
+
+    function _collectCharacter(character, callback)
+    {
+      var name = character.get('name');
+      var avatar = character.get('avatar');
+      var notes = null;
+      var gmnotes = null;
+      character.get('bio', function(returnedNotes) {
+        notes = returnedNotes;
+        character.get('gmnotes', function(returnedGmnotes) {
+          gmnotes = returnedGmnotes;
+          let characterData = new ObjectData(name, avatar, notes, gmnotes);
+          callback(characterData);
+        });
+      });
     }
 
     function _showHandoutToPlayer(sendFrom, sendTo, handout)
@@ -154,7 +243,8 @@ var ShowToChatScript = ShowToChatScript || (function() {
       return fullHtml;
     }
 
-    function _getHandoutByName(name) {
+    function _getHandoutByName(name)
+    {
       var results = findObjs({
         _type: "handout",
         _name: name
@@ -163,6 +253,39 @@ var ShowToChatScript = ShowToChatScript || (function() {
         return null;
       }
       return results[0];
+    }
+
+
+    // ---------------------------------------------
+    // INNER CLASSES
+    // ---------------------------------------------
+    ObjectData = function(n, a, t, g)
+    {
+      this.name = n;
+      this.avatar = a;
+      this.text = t;
+      this.gmnotes = g;
+
+      function getHtml()
+      {
+        var fullHtml = "";
+        // for characters, I received "null" (string) instead of null (empty/undefined)
+        // so in case, I remove them all!
+        if (this.name && 'null' != this.name) {
+          fullHtml +=  `<div style="box-shadow: 3px 3px 2px #888888; font-family: Verdana; text-shadow: 2px 2px #000; text-align: center; vertical-align: middle; padding: 1px 1px; margin-top: 0.1em; border: 1px solid #000; border-radius: 8px 8px 8px 8px; color: #FFFFFF; background-color:#666666;">${this.name}</div>`;
+        }
+        if (this.avatar && 'null' != this.avatar) {
+          fullHtml += `<div style="box-shadow: 3px 3px 2px #888888; text-align: center; vertical-align: middle; padding: 1px 1px; margin-top: 0.1em; border: 1px solid #000; color: #FFFFFF; background-color:#ffffff;"><img src="${this.avatar}" /></div>`;
+        }
+        if (this.text && 'null' != this.text) {
+            fullHtml +=  `<div style="box-shadow: 3px 3px 2px #888888; padding: 1px 1px; margin-top: 0.1em; border: 1px solid #000; background-color:#ffffff;">${this.text}</div>`;
+        }
+        if (this.gmnotes && 'null' != this.gmnotes) {
+            fullHtml +=  `<div style="box-shadow: 3px 3px 2px #888888; padding: 1px 1px; margin-top: 0.1em; border: 1px solid #000; background-color:#dddddd;">${this.gmnotes}</div>`;
+        }
+        fullHtml = `<div class="handout2chat">${fullHtml}</div>`;
+        return fullHtml;
+      }
     }
 
     // ---------------------------------------------
@@ -174,6 +297,7 @@ var ShowToChatScript = ShowToChatScript || (function() {
       showToPlayers: showToPlayers
     };
 }());
+
 
 // ---------------------------------------------
 // LAUNCH
